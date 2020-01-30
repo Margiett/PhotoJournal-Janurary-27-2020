7145a89e8e876d228665767a068c41cadf71167c
Margiett Gil <mxg627@icloud.com>
2020-01-29 20:07:09 -0500

    created longpress function

--- PhotoJournal Janurary 27 2020/PhotoCVC.swift
+++ PhotoJournal Janurary 27 2020/PhotoCVC.swift
@@ -16,6 +16,29 @@ class PhotoCVC: UICollectionViewCell {
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var editButton: UIButton!
     
+    
+   
+    //MARK: Step 2. long press setup
+    // setup long press gesture recognizer
+    private lazy var longPressGesture: UILongPressGestureRecognizer = {
+        let actionPress = UILongPressGestureRecognizer()
+        //MARK: This code is not going to work unless you create Step 1 long press setup
+        actionPress.addTarget(self, action: #selector(longPressAction(gesture:)))
+        return actionPress
+    }()
+    
+    //MARK: Step 1: to make long pressaction work
+        @objc
+        private func longPressAction(gesture: UILongPressGestureRecognizer){
+            
+            if gesture.state == .began { // if gesture is actived (touched)
+                gesture.state = .cancelled
+                return
+                
+            }
+        }
+ 
+    
     func configureCell(for item: T){
         titileLabel.text = item.title
         dateLabel.text = item.date
